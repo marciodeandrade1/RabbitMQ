29b438faf85ffe69579a75cfd408fc953bf5d9f5
Marcio de Andrade <marcio@correnteeletrica.com.br>
2022-05-09 18:37:25 -0400

    Queue
    Demo Queue

---
+++ DemoExchange/DemoQueue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace DemoQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            //Ponto de entrada para o RabbitMQ
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            //Criando Queue
+            model.QueueDeclare("demoqueue", true, false, false, null);
+            Console.WriteLine("Criando Queue");
+            Console.ReadLine();
+        }
+    }
+}

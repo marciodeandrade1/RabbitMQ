b64aad2435029a71be9057802cf506b9d0180159
Marcio de Andrade <marcio@correnteeletrica.com.br>
2022-05-09 19:06:26 -0400

    Binding

---
+++ DemoExchange/DemoQueueBinding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace DemoQueueBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            model.QueueBind("demoqueue", "demoExchange", "directexchange_key");
+            Console.WriteLine("Criando Binding");
+            Console.ReadLine();
+        }
+    }
+}

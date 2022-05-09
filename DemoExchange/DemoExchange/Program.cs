5d91a6be00d548be1194c7958beeb0847e66b605
Marcio de Andrade <marcio@correnteeletrica.com.br>
2022-05-09 17:38:37 -0400

    demoExchange
    Criando o projeto e Add Exchange

---
+++ DemoExchange/DemoExchange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace DemoExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            //Criação Exchange
+            model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
+            Console.WriteLine("Criado Exchange");
+            Console.ReadKey();
+        }
+    }
+}

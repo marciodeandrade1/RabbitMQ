55bbbe8cd7ccfa8b639f40fb6c16ebb07b7a3437
Marcio de Andrade <marcio@correnteeletrica.com.br>
2022-05-10 09:26:27 -0400

    Consumer

---
+++ DemoExchange/RabbitMQConsumer/MessageReceiver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQConsumer
+{
+        public class MessageReceiver : DefaultBasicConsumer
+        {
+            private readonly IModel _channel;
+            public MessageReceiver(IModel channel)
+            {
+                _channel = channel;
+            }
+        public void HandleBasicDeliver(string consumerTag, ulong deliveryTag, bool redelivered, string exchange, string routingKey, IBasicProperties properties, byte[] body)
+            {
+                Console.WriteLine($" ===== Consumindo Menssagem ====");
+                Console.WriteLine(string.Concat("Menssagem recebida do exchange ", exchange));
+                Console.WriteLine(string.Concat("Consumidor tag: ", consumerTag));
+                Console.WriteLine(string.Concat("Entregue tag: ", deliveryTag));
+                Console.WriteLine(string.Concat("Roteamento tag: ", routingKey));
+                Console.WriteLine(string.Concat("Menssagem: ", Encoding.UTF8.GetString(body)));
+                _channel.BasicAck(deliveryTag, false);
+            }
+        }
+    }
---
+++ DemoExchange/RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string HostName = "localhost";
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password,
+            };
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+            // accept only one unack-ed message at a time
+            // uint prefetchSize, ushort prefetchCount, bool global
+            channel.BasicQos(0, 1, false);
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("demoqueue", false, messageReceiver);
+            Console.ReadLine();
+        }
+    }
+}
